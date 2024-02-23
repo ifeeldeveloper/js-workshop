910a123cf576da3e1a0b301368ddb1150a4d5d43
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-23 19:20:15 +0545

    tut2.js Updated

--- Day2/tut2.js
+++ Day2/tut2.js
@@ -2,16 +2,15 @@ var gilas = "paani";
 let belya = "Saag";
 const dontManipulate = "Ask First";
 
-
 if (true) {
-  var gilas = "Chaha"
+  var gilas = "Chaha";
   let belya = "Chauchau";
-  const dontManipulate = "Error"
+  const dontManipulate = "Surprise";
 
-  console.log(gilas)
-  console.log(belya)
-  console.log(dontManipulate)
+  console.log(gilas, "Declared in Block Scope of if and takes value from Block Scope")
+  console.log(belya, "Declared in Block Scope of if and takes value from Block Scope")
+  console.log(dontManipulate, "Declared in Block Scope of if and takes value from Block Scope")
 }
-console.log(gilas)
-console.log(belya)
-console.log(dontManipulate)
+console.log(gilas, "Declared in Global Scope but takes value from Block Scope of if")
+console.log(belya, "Declared in Global Scope and takes value from Global Scope")
+console.log(dontManipulate, "Declared in Global Scope and takes value from Global Scope")
