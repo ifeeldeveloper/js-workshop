c2e18a8327d56022eb82f57fcf4f0e25976f6547
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-23 22:22:39 +0545

    tut1.js updated

--- Day2/tut1.js
+++ Day2/tut1.js
@@ -1,2 +1,30 @@
 console.log("Welcome to JavaScript")
 console.log("Welcome to JagaScript")
+
+let test = "First Variable";
+test = 2;
+test = true;
+let arr = ["Jagadish", 'Jaggu', "Jiwan", 'Bhoj', 'Bhaskar', 'Don']
+
+
+let name = "Jagadish";
+const age = 24;
+isValid = true;
+// name=12;
+// console.log(name + 1)
+
+let itsMe = {
+  name: "Jagadish Bhatta",
+  age: 23
+}
+
+console.log(test)
+console.log(arr)
+console.log(arr[1], arr[5])
+console.log(name + " Bhatta" + " I am " + age + " Years old" + " and its not " + isValid);
+
+console.log("Hey my name is " + name + " And I am " + age + " Years old")
+
+console.log("Hey my name is " + arr[2] + " And I am " + age + " Years old")
+
+console.log("Hey my name is " + itsMe.name + " And I am " + itsMe.age + " Years old")
