5cd4d67db3447776429ace96bd1583c20821b772
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-23 18:52:39 +0545

    tut2.js uploaded

---
+++ Day2/tut2.js
@@ -0,0 +1,17 @@
+var gilas = "paani";
+let belya = "Saag";
+const dontManipulate = "Ask First";
+
+
+if (true) {
+  var gilas = "Chaha"
+  let belya = "Chauchau";
+  const dontManipulate = "Error"
+
+  console.log(gilas)
+  console.log(belya)
+  console.log(dontManipulate)
+}
+console.log(gilas)
+console.log(belya)
+console.log(dontManipulate)
